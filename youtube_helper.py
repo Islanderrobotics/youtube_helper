8660cf1d8ec48b7720eefea067709c8761a5aaf5
William McKeon <islanderrobotics@gmail.com>
2021-05-13 23:01:38 -0400

    still have to work on the insert if on line 43

--- youtube_helper.py
+++ youtube_helper.py
@@ -2,6 +2,7 @@ class helper:
     def __init__(self):
         self.keywords = {} #changed this
         self.key = [] # added this
+        self.key.append("keyword")#added this
         self.keywords["keyword"] = []#added this
         print("hello youtuber i am the youtube helper but you can call me helper")
         print("I was created to make your youtube keyword research less of a head ach")
@@ -9,36 +10,40 @@ class helper:
         print("as well as i have the abilty to export all of your keywords in a comma seperated format")
         print("so that you have the ability to copy and paste it to youtube")
         print("wow that was a mouth full")
+        count = int(input("how many databases will you like to enter"))
+        i = 0
         length = True
-        while(length):
+        while(i<count):
             database = input("what will the name of the database be")
             self.keywords[database] = [] #changed this
             self.key.append(database) #added this
-            user = input("would you like to enter another databases name if so please type yes")
-            if (user.upper() == "YES"):
-                pass
-            #could have put length = True if that makes it easier for you
-            else:
-                length = False
+            i+=1
     def insert(self):
         print("Hello youtuber now that we have gotten that out of the way why dont we start adding"
-              "some keywrds")
-        size = None
+              "some keywords")
+        size = 0
         count = 0
         user_decsion = input("is there a specifc number of keywords you would like to enter"
-                             "please type yes if there is or any key to continue on")
+                             " please type yes if there is or any key to continue on")
         if user_decsion.upper() == "YES":
             size = int(input("how many keywords do you want to enter"))
 
         keep_going = True
         while (keep_going):
-            if (size != None):
+            if (size > 0):
                 count+=1
                 if (size == count):
                     print("seems you have reached your limit")
-                    break
+                    keep_going = False
             for key in self.key:
                 if key == "keyword":
                     self.keywords[key].append(input("what will the keyword be"))
                 else:
                     self.keywords[key].append(int(input(f"what will the score from {key} be")))
+            if (size ==0):
+                data = input("would you like to add more keywords please enter yes if you do"
+                             " or any key if you would not like to")
+                if (data.upper() == "YES"):
+                    pass
+                else:
+                    keep_going = False
