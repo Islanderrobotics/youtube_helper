1fa9e879dd592b4e488cd11f2fc25129e03c8606
William McKeon <islanderrobotics@gmail.com>
2021-05-13 23:05:21 -0400

    use this version for the code

--- youtube_helper.py
+++ youtube_helper.py
@@ -7,13 +7,9 @@ class helper:
         print("as well as i have the abilty to export all of your keywords in a comma seperated format")
         print("so that you have the ability to copy and paste it to youtube")
         print("wow that was a mouth full")
-        length = True
-        while(length):
+        count = int(input("how many databases will you like to enter"))
+        i = 0
+        while (i < count):
             database = input("what will the name of the database be")
             self.keywords.append(database)
-            user = input("would you like to enter another databases name if so please type yes")
-            if (user.upper() == "YES"):
-                pass
-            #could have put length = True if that makes it easier for you
-            else:
-                length = False
+            i+=1
