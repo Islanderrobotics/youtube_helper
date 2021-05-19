54f4198e67ce583184b25a4ee32a85ffaf07d480
William McKeon <islanderrobotics@gmail.com>
2021-05-19 18:10:54 -0400

    finished the video

--- islander_helper.py
+++ islander_helper.py
@@ -1,8 +1,11 @@
-keyword = []
+
 
 class helper:
     def __init__(self):
-        # keyword_2 = []
+        self.keyword = {}
+        self.key = []
+        self.keyword["Keyword"] = []
+        self.key.append("Keyword")
         print("Hello youtuber I am the youtube keyword planner but you can call me helper if you would like.")
         print("I was created to make your youtube keyword research less of a head ache.")
         print("I do this by keeping track of all your keyword scores from different databases for each video")
@@ -15,6 +18,33 @@ class helper:
         count = int(input("with all of that being said how many databases will we be using today"))
         for i in range(count):
             databases = input("what is the name a database we will be looking at today")
-            keyword.append(databases)
-        for data in keyword:
-            print(data)
+            self.keyword[databases] = []
+            self.key.append(databases)
+
+    def insert(self):
+        print("Hello Youtuber now that we have the databases that you will be using why dont we start adding some keywords")
+        size = 0
+        count = 0
+        user_decsion = input("is there a specifc number of keywords you would like to enter please type yes if there is or any key if not")
+        if (user_decsion.upper() == "YES"):
+            size = int(input("how many keywords will you be entering"))
+        keep_going = True
+        while(keep_going):
+            for key in self.key:
+                if key == "Keyword":
+                    self.keyword[key].append(input("what keyword will you like to enter"))
+                else:
+                    self.keyword[key].append(int(input(f"what is the score from {key} for that keyword")))
+            if(size == 0):
+                user_decsion = input("please type yes if you would like to enter more keywords or any key to continue")
+                if (user_decsion.upper() == "YES"):
+                    pass
+                else:
+                    keep_going = False
+            if(size == count+1):
+                print("seems you have reached your keyword limit")
+                keep_going = False
+            if(size>0):
+                count+=1
+    def driver(self):
+        self.insert()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from islander_helper import helper
 
 key = helper()
+key.driver()
